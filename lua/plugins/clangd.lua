6b81a62ade59c3d0777c8b0b311c0516594d1ad7
Artem Belevich <artemb@gmail.com>
2024-01-16 00:29:38 -0800

    Removed debugging print

--- lua/plugins/clangd.lua
+++ lua/plugins/clangd.lua
@@ -23,7 +23,6 @@ return {
               )(fname)
           end,
           on_attach = function(client, bufnr)
-            print("on_attach")
             require("clangd_extensions.inlay_hints").setup_autocmd()
             require("clangd_extensions.inlay_hints").set_inlay_hints()
           end,
